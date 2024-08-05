d16fccbe61523ebf4cd57b1dee6b84f9255cae17
4multimedia <biuro@4multimedia.pl>
2024-08-05 09:53:20 +0200

    1.0.3

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+import t from'react';import{StyleSheet as e,TextInput as r}from'react-native';var a=()=>{};function n(){return t.createElement(r,{style:i.input})}var i=e.create({input:{height:40,margin:12,borderWidth:1,padding:10}});export{n as InputText,a as useForm};
+//# sourceMappingURL=index.js.map
